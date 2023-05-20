0d6cfafba2e0a25003b62a45555cb8fc5eba6645
jsungl <wotjd931026@gmail.com>
2023-05-20 23:33:25 +0900

    Initial commit

---
+++ src/test/java/hello/springcommunity/SpringCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.springcommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
