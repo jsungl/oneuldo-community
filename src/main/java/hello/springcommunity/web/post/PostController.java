f3642a22aab0b0e088aca5dc2ac4cd7948f1543a
jsungl <wotjd931026@gmail.com>
2023-07-07 23:31:42 +0900

    게시판 프로젝트 - 게시물 등록,조회,상세 조회 기능 수정

--- src/main/java/hello/springcommunity/web/post/PostController.java
+++ src/main/java/hello/springcommunity/web/post/PostController.java
@@ -1,13 +1,17 @@
 package hello.springcommunity.web.post;
 
+import hello.springcommunity.domain.member.Member;
 import hello.springcommunity.domain.post.Post;
 import hello.springcommunity.domain.post.PostService;
 import hello.springcommunity.domain.post.PostUpdateDto;
+import hello.springcommunity.web.SessionConst;
+import hello.springcommunity.web.post.form.PostSaveForm;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -32,7 +36,8 @@ public class PostController {
     //상세
     @GetMapping("/{postId}")
     public String post(@PathVariable long postId, Model model) {
-        Post post = postService.findById(postId).get();
+//        Post post = postService.findById(postId).get();
+        Post post = postService.findOne(postId).orElseThrow();
         model.addAttribute("post", post);
         return "posts/post";
     }
@@ -40,33 +45,34 @@ public class PostController {
 
     //등록폼
     @GetMapping("/add")
-    public String addForm(Model model) {
-        model.addAttribute("post", new Post());
+    public String addForm(@ModelAttribute("postForm") PostSaveForm postForm) {
         return "posts/addForm";
     }
 
     @PostMapping("/add")
-    public String addPost(@ModelAttribute("post") Post post, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+    public String addPost(@Validated @ModelAttribute("postForm") PostSaveForm postForm, BindingResult bindingResult,
+                          @SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginMember, RedirectAttributes redirectAttributes) {
 
         if (bindingResult.hasErrors()) {
             log.info("errors={}", bindingResult);
             return "posts/addForm";
         }
 
-        log.info("post={}", post); //id와 등록날짜는 null
+        log.info("postForm={}", postForm); //id와 등록날짜는 null
 
-        Post savedPost = postService.save(post);
+        Post savedPost = postService.save(postForm.getTitle(), postForm.getContent(), loginMember.getId()); //등록한 게시물의 제목,내용,작성자 를 담아서 전달
         log.info("savedPost={}", savedPost); //IDENTITY 방식에 의해 DB에 저장후 id 값과 등록날짜(regDate)를 확인할 수 있다
 
         redirectAttributes.addAttribute("postId", savedPost.getId());
         redirectAttributes.addAttribute("status", true);
-        return "redirect:/posts/{postId}"; //상세페이지로 이동
+        return "redirect:/posts/{postId}"; //해당 게시물의 상세페이지로 이동
     }
 
     //수정폼
     @GetMapping("/{postId}/edit")
     public String editForm(@PathVariable Long postId, Model model) {
-        Post post = postService.findById(postId).get();
+//        Post post = postService.findById(postId).get();
+        Post post = postService.findOne(postId).get();
         model.addAttribute("post", post);
         return "posts/editForm";
     }
--- src/main/java/hello/springcommunity/web/post/form/PostSaveForm.java
+++ src/main/java/hello/springcommunity/web/post/form/PostSaveForm.java
@@ -1,26 +1,29 @@
 package hello.springcommunity.web.post.form;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 /**
- * 폼 데이터 전달을 위한 별도의 객체 사용
+ * 게시물 등록용 폼 객체
  * 실무에서는 회원 등록시 회원과 관련된 데이터만 전달받는것이 아니라, 추가로 많은 부가 데이터가 넘어온다
  * 그래서 보통 Post 을 직접 전달받는것이 아니라, 복잡한 폼의 데이터를 컨트롤러까지 전달할 별도의 객체를 만들어서 전달한다
  * 여기서는 PostSaveForm 이라는 폼을 전달받는 전용 객체를 만들어서 @ModelAttribute 로 사용한다
  * 이것을 통해 컨트롤러에서 폼 데이터를 전달받고, 이후 컨트롤러에서 필요한 데이터를 사용해서 Post 을 생성한다
  */
-@Data
+
+@Getter
+@Setter
 public class PostSaveForm {
 
-    @NotBlank
+    @NotBlank(message = "제목을 입력해주세요")
     private String title;
 
-    @NotBlank
-    private String description;
-
-    @NotBlank
-    private String author;
+//    @NotBlank
+    @NotNull //내용은 아무것도 입력하지 않았을 때 발생할 수 있는 Null pointer exception 을 방지
+    private String content;
 
 }
