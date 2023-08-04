560670fe534b4bfab0201551ceece09322078b15
jsungl <wotjd931026@gmail.com>
2023-08-04 23:11:45 +0900

    게시판 프로젝트 - 필요없는 파일 삭제

--- src/main/java/hello/springcommunity/domain/item/ItemRepositoryV2.java
+++
@@ -1,9 +0,0 @@
-package hello.springcommunity.domain.item;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-/**
- * 기본 CRUD 와 단순 조회는 JpaRepository 를 상속받은 ItemRepositoryV2 가 스프링 데이터 JPA 기능을 사용하여 처리
- */
-public interface ItemRepositoryV2 extends JpaRepository<Item, Long> {
-}
