72c191ac03d8356598adf065c74cbd04ea424f16
jsungl <wotjd931026@gmail.com>
2023-12-14 03:23:13 +0900

    게시판 프로젝트 - 유효성 조건 수정

--- src/main/java/hello/springcommunity/dto/member/MemberSaveRequestDTO.java
+++ src/main/java/hello/springcommunity/dto/member/MemberSaveRequestDTO.java
@@ -32,7 +32,7 @@ public class MemberSaveRequestDTO {
     private String rePassword;
 
     @NotBlank(groups = NotBlankGroup.class)
-    @Pattern(regexp = "^[가-힣a-zA-Z0-9]{2,10}$", message = "닉네임은 특수문자를 포함하지 않은 2~10자리여야 합니다.", groups = PatternGroup.class)
+    @Pattern(regexp = "^[가-힣a-zA-Z0-9]{2,8}$", message = "닉네임은 특수문자를 포함하지 않은 2~8자리여야 합니다.", groups = PatternGroup.class)
     private String nickname;
 
     @NotBlank(groups = NotBlankGroup.class)
--- src/main/java/hello/springcommunity/dto/post/PostRequestDTO.java
+++ src/main/java/hello/springcommunity/dto/post/PostRequestDTO.java
@@ -1,11 +1,11 @@
 package hello.springcommunity.dto.post;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 /**
  * 게시물 등록용 폼 객체(DTO)
@@ -20,11 +20,12 @@ import javax.validation.constraints.NotBlank;
 @NoArgsConstructor
 public class PostRequestDTO {
 
-    @NotBlank(message = "제목을 입력해주세요")
+    @NotBlank(message = "제목을 입력해주세요.")
+    @Size(max = 40, message = "제목은 40자 이하로 입력해주세요.")
     private String title;
 
     //@NotNull //내용은 아무것도 입력하지 않았을 때 발생할 수 있는 Null pointer exception 을 방지
-    @NotBlank(message = "내용을 입력해주세요")
+    @NotBlank(message = "내용을 입력해주세요.")
     private String content;
 
 }
